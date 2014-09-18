920db8a0d8240cbe015f95bb7318b9436cf1c45c
Abhishek Jain <abhi2000@gmail.com>
2014-09-18 12:58:34 +0100

    Added hasher js to create salt and hash for password security

--- server/api/auth/auth.spec.js
+++ server/api/auth/auth.spec.js
@@ -3,8 +3,48 @@
 var should = require('should');
 var app = require('../../app');
 var request = require('supertest');
+var data = require('../../data');
+var hasher = require('./hasher');
 
 describe('POST /auth', function () {
+    var salt = hasher.createSalt(12);
+    before(function (done) {
+        data.users.remove({}, function (err) {
+            if (!err)
+                console.log("users removed");
+            else
+                console.log(err);
+            done();
+        });
+    });
+
+    beforeEach(function (done) {
+
+        data.users.add({
+            email: 'a@a.com',
+            provider: 'local',
+            role: 'user',
+            name: 'test user',
+            salt: salt,
+            password: hasher.computeHash("abc", salt)
+        }, function (err) {
+            if (!err)
+                console.log("user added");
+            else
+                console.log(err);
+            done();
+        });
+    });
+
+    afterEach(function (done) {
+        data.users.remove({email: 'a@a.com'}, function (err) {
+            if (!err)
+                console.log("user added");
+            else
+                console.log(err);
+            done();
+        });
+    });
 
     it('should respond with JSON array', function (done) {
         request(app)
@@ -12,8 +52,8 @@ describe('POST /auth', function () {
             .send(
             {
                 user: {
-                    id: "id",
-                    password: "password"
+                    email: "a@a.com",
+                    role: "admin"
                 }
             })
             .expect(200)
@@ -27,3 +67,74 @@ describe('POST /auth', function () {
             });
     });
 });
+
+describe('hasher', function () {
+    it('createSalt should return a salt', function (done) {
+        var salt = hasher.createSalt();
+        should.exist(salt);
+        done();
+    });
+
+    it('createSalt create random bytes as long as the passed integer', function (done) {
+        var salt = hasher.createSalt(8);
+        (salt.length).should.be.exactly(8);
+
+        salt = hasher.createSalt(10);
+        (salt.length).should.be.exactly(10);
+        done();
+    });
+
+    it('createSalt create random bytes of 8 characters long if no paramaters passed', function (done) {
+        var salt = hasher.createSalt();
+        (salt.length).should.be.exactly(8);
+
+        done();
+    });
+
+    it('computeHash create a hash', function (done) {
+        var salt = hasher.createSalt(8);
+        var hash = hasher.computeHash("abc", salt);
+        should.exist(hash);
+
+        done();
+    });
+
+    it('computeHash create the same hash for the same string and salt', function (done) {
+        var salt = hasher.createSalt(8);
+        var hash1 = hasher.computeHash("abc", salt);
+        var hash2 = hasher.computeHash("abc", salt);
+        hash1.should.be.exactly(hash2);
+
+        done();
+    });
+
+    it('computeHash create different hash for the same string but different salt', function (done) {
+        var salt1 = hasher.createSalt(8);
+        var salt2 = hasher.createSalt(10);
+        var hash1 = hasher.computeHash("abc", salt1);
+        var hash2 = hasher.computeHash("abc", salt2);
+        hash1.should.not.be.exactly(hash2);
+
+        done();
+    });
+
+    it('computeHash create different hash for the different string and same salt', function (done) {
+        var salt = hasher.createSalt(8);
+        var hash1 = hasher.computeHash("xyz", salt);
+        var hash2 = hasher.computeHash("abc", salt);
+        hash1.should.not.be.exactly(hash2);
+
+        done();
+    });
+
+    it('computeHash create different hash for different string and salt', function (done) {
+        var salt1 = hasher.createSalt(8);
+        var salt2 = hasher.createSalt(10);
+        var hash1 = hasher.computeHash("xyz", salt1);
+        var hash2 = hasher.computeHash("abc", salt2);
+        hash1.should.not.be.exactly(hash2);
+
+        done();
+    });
+});
+
